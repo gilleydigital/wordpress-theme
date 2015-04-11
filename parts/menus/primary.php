8195ae8639cf800281d4acd47272d7eef36bf6fb
Chris Gilley <chris@gilleydigital.com>
2015-04-11 08:13:44 -0400

    Standardized primary menu part

--- parts/menus/primary.php
+++ parts/menus/primary.php
@@ -1,9 +1,8 @@
-<nav class="primary">
-	<?php 
-		wp_nav_menu(array(
-			'theme_location' => 'primary',
-			'depth' => 2,
-			'container' => false
-		));
-	?>
-</nav>
+<?php 
+	wp_nav_menu(array(
+		'theme_location' => 'primary',
+		'depth' => 2,
+		'container' => 'div',
+		'container_class' => 'primary'
+	));
+?>
