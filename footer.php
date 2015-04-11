1e365d889d6511815817216690635386153e2f00
Chris Gilley <chris@gilleydigital.com>
2015-04-11 18:24:45 -0400

    Added blog name to footer

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 	<div class="footer">
 		<div class="container">
 			<div class="footer-content content">
-				<p class="copyright">&copy; <?php echo date('Y'); ?></p>
+				<p class="copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( $show ); ?></p>
 			</div>
 			<?php get_template_part('parts/menus/utility'); ?>
 			<?php get_template_part('parts/menus/social'); ?>
