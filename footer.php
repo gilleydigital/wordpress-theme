9a622ffc3011b36130317ad20b4bbb335082b77c
Chris Gilley <chris@gilleydigital.com>
2015-08-28 16:34:13 -0400

    Removed search form from default footer

--- footer.php
+++ footer.php
@@ -4,7 +4,6 @@
 		<div class="container">
 			<div class="footer-content content">
 				<p class="copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( $show ); ?></p>
-				<?php get_template_part('parts/search-form'); ?>
 			</div>
 			<?php get_template_part('parts/menus/utility'); ?>
 			<?php get_template_part('parts/menus/social'); ?>
