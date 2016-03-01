9a98e004d4090c9da4fbd14617e38c00e017bd0f
Chris Gilley <chris@gilleydigital.com>
2016-03-01 12:23:42 -0500

    Mobile pop open menu

--- js/script.js
+++ js/script.js
@@ -4,6 +4,28 @@ jQuery(function($){
 		$('#mobile-menu, #mobile-toggle').toggle();
 	});
 	
+	// Mobile
+	$("#mobile-menu .menu li.menu-item-has-children > a").click(function() {
+		var parent = $(this).parent();
+		
+		if (parent.data('status') === 'open')
+		{
+			// Close it
+			parent.removeClass('open');
+			parent.data('status', 'closed');
+			parent.children('.sub-menu').hide();
+		}
+		else
+		{
+			// Open it
+			parent.addClass('open');
+			parent.data('status', 'open');
+			parent.children('.sub-menu').show();
+		}
+
+		return false;
+	});
+	
 	// Whole area clickable
 	$(".big-link").click(function() {
 		window.location = $(this).find("a").attr("href");
