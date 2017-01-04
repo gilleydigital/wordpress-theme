94cd31128f6605168a31d17eb45287a0333355c5
Chris Gilley <chris@gilleydigital.com>
2017-01-04 15:11:06 -0500

    Deleted less folder and cleaned up index

--- index.php
+++ index.php
@@ -2,9 +2,6 @@
 
 <div class="body">
 	<div class="body-content content">
-		<h1>Headline</h1>
-		<h2>Test okay</h2>
-		<p>Donec sollicitudin molestie malesuada. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Donec sollicitudin molestie malesuada. Vivamus suscipit tortor eget felis porttitor volutpat. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Sed porttitor lectus nibh. Vivamus suscipit tortor eget felis porttitor volutpat. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Donec rutrum congue leo eget malesuada. Pellentesque in ipsum id orci porta dapibus.</p>
 		<?php
 			if (have_posts()):
 				while (have_posts()): 
@@ -13,7 +10,6 @@
 				endwhile;
 			endif;
 		?>
-		<div class="map"></div>
 	</div>
 </div>
 
